9273254fd6d93364524dd69a8aa50e71a2d95c71
Shivang <shivangkar@Shivangs-MacBook-Air.local>
2020-09-30 01:09:37 +0530

    Changed package name for releasing on atmosphere

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "raix:eventstate",
+  name: "shivangkar:eventstate",
   version: '0.0.5',
   summary: "A server and client event state package.",
   git: "https://github.com/raix/Meteor-EventState.git"
@@ -9,7 +9,7 @@ Package.onUse(function (api) {
 
   api.versionsFrom('1.0');
 
-  api.use(['raix:eventemitter@1.0.0']);
+  api.use(['shivangkar:eventemitter@1.0.0']);
 
   api.addFiles('eventstate.common.js', ['client', 'server']);
 
@@ -18,7 +18,7 @@ Package.onUse(function (api) {
 
 
 Package.onTest(function (api) {
-  api.use(['raix:eventstate']);
+  api.use(['shivangkar:eventstate']);
   api.use('test-helpers', ['server', 'client']);
   api.use('tinytest');
 
